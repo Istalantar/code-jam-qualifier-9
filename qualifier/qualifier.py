58339ac269db8567848c3cceeba7dfdc7215f174
Istalantar <istalantar@gmail.com>
2022-07-19 22:19:48 +0200

    adds qualifier solution

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -31,4 +31,32 @@ class RestaurantManager:
             Request object containing information about the sent
             request to your application.
         """
-        ...
+
+        if request.scope['type'] == 'staff.onduty':
+            self.staff[request.scope['id']] = request
+        elif request.scope['type'] == 'staff.offduty':
+            self.staff.pop(request.scope['id'])
+        elif request.scope['type'] == 'order':
+            full_order = await request.receive()
+            found = self.__find_staff_member(request.scope['speciality'])
+            await found.send(full_order)
+            result = await found.receive()
+            await request.send(result)
+            # add staff member back to roster once done
+            self.staff[found.scope['id']] = found
+        else:
+            raise Exception('Unknown request type')
+
+    def __find_staff_member(self, speciality) -> Request:
+        """Get the next available staff member, preferably one for speciality.
+
+        :speciality: speciality of the next order
+        """
+
+        for key, staff_member in self.staff.copy().items():
+            if speciality in staff_member.scope['speciality']:
+                self.staff.pop(key)
+                return staff_member
+
+        # If no staff for specialty is found, get first staff member from dict
+        return self.staff.pop(next(iter(self.staff)))
